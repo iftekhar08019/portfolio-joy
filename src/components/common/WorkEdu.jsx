117ac5b16acb51163e857bd11ac8fdd5a475bbac
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-10-25 10:00:48 +0200

    work experience added

--- src/components/common/WorkEdu.jsx
+++ src/components/common/WorkEdu.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DuoneosLogo from "../../assets/duoneos_logo.jpeg";
+import InterherenceLogo from "../../assets/interherence_logo.jpeg";
 const ExperienceTabs = () => {
   const [activeTab, setActiveTab] = useState("work");
 
@@ -34,32 +35,60 @@ const ExperienceTabs = () => {
             {/* Continuous vertical line */}
             <div className="absolute left-4 lg:left-5 top-0 w-0.5 bg-gray-600 h-full"></div>
             
-            <div className="flex items-start gap-3 lg:gap-5">
-              <div className="relative z-10">
-                <div className="bg-gray-700 p-1 rounded-md w-8 h-8 lg:w-10 lg:h-10 flex-shrink-0">
-                  <img
-                    src={DuoneosLogo}
-                    className="w-full h-full object-cover rounded-md"
-                    alt="Duoneos Logo"
-                  />
+            <div className="space-y-6 lg:space-y-10">
+              <div className="flex items-start gap-3 lg:gap-5">
+                <div className="relative z-10">
+                  <div className="bg-gray-700 p-1 rounded-md w-8 h-8 lg:w-10 lg:h-10 flex-shrink-0">
+                    <img
+                      src={InterherenceLogo}
+                      className="w-full h-full object-cover rounded-md"
+                      alt="Interherence Logo"
+                    />
+                  </div>
+                </div>
+                <div>
+                  <div className="text-xs lg:text-sm text-gray-400">
+                    <p>September 2022 - March 2023</p>
+                    <h2 className="text-sm lg:text-base font-bold text-white">Interherence GmbH</h2>
+                    <p>Working Student – Designing</p>
+                    <p>Erlangen, Germany</p>
+                  </div>
+                  <ul className="list-disc pl-3 mt-2 lg:mt-3 space-y-1 lg:space-y-2 text-xs lg:text-sm text-gray-100">
+                    <li>Designed product marketing materials using Photoshop/Illustrator ensuring brand consistency</li>
+                    <li>Developed Smart Substrate cleaning manual, reducing user maintenance queries by ~20%</li>
+                    <li>Created event banners with Canva, increasing program visibility by over 25%</li>
+                  </ul>
                 </div>
               </div>
-              <div>
-                <div className="text-xs lg:text-sm text-gray-400">
-                  <p>May 2021 - October 2021 </p>
-                  <h2 className="text-sm lg:text-base font-bold text-white">Duoneos</h2>
-                  <p>Frontend Developer Intern</p>
+              
+              <div className="flex items-start gap-3 lg:gap-5">
+                <div className="relative z-10">
+                  <div className="bg-gray-700 p-1 rounded-md w-8 h-8 lg:w-10 lg:h-10 flex-shrink-0">
+                    <img
+                      src={DuoneosLogo}
+                      className="w-full h-full object-cover rounded-md"
+                      alt="Duoneos Logo"
+                    />
+                  </div>
                 </div>
-                <ul className="list-disc pl-3 mt-2 lg:mt-3 space-y-1 lg:space-y-2 text-xs lg:text-sm text-gray-100">
-                  <li>Developed user-friendly web applications using React</li>
-                  <li>
-                    Collaborated with designers to implement responsive UI/UX
-                  </li>
-                  <li>
-                    Participated in code reviews and contributed to team knowledge
-                    sharing
-                  </li>
-                </ul>
+                <div>
+                  <div className="text-xs lg:text-sm text-gray-400">
+                    <p>May 2021 - October 2021 </p>
+                    <h2 className="text-sm lg:text-base font-bold text-white">Duoneos</h2>
+                    <p>Frontend Developer Intern</p>
+                    <p>Chittagong, Bangladesh</p>
+                  </div>
+                  <ul className="list-disc pl-3 mt-2 lg:mt-3 space-y-1 lg:space-y-2 text-xs lg:text-sm text-gray-100">
+                    <li>Developed user-friendly web applications using React</li>
+                    <li>
+                      Collaborated with designers to implement responsive UI/UX
+                    </li>
+                    <li>
+                      Participated in code reviews and contributed to team knowledge
+                      sharing
+                    </li>
+                  </ul>
+                </div>
               </div>
             </div>
           </div>
